0e6409e33c433b382bc26b95c7fc2fa3b9373461
zMag33z <mag33.cdh@gmail.com>
2023-01-28 21:16:27 -0600

    view all employees database working

--- db/query-db/employees/view.js
+++ db/query-db/employees/view.js
@@ -1,8 +1,17 @@
 const db = require("../../../connect/localize-db");
 
 function viewallemployees(){
-    console.log(`i'm in`);
-
+    return db.promise()
+    .query(`
+SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS employee, r.title, d.department, r.salary,CONCAT (m.first_name, ' ', m.last_name) AS manager
+FROM employee e
+LEFT JOIN role r ON e.role_id = r.id
+LEFT JOIN department d on d.id = r.dept_id
+LEFT JOIN employee m ON m.id = e.manager_id;`)
+    .then(([collected]) => {
+        return console.table(``, collected);
+    })
+    .catch(err => { console.log(err) });
 }
 
 module.exports = viewallemployees;
--- db/seeds.sql
+++ db/seeds.sql
@@ -18,13 +18,23 @@ VALUES
   ('Sales Manager', 4, 100000),
   ('Sales Associate', 4, 80000);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id, is_manager)
+INSERT INTO employee 
+(first_name,
+ last_name,
+  role_id,
+   manager_id,
+    is_manager)
+    
 VALUES
-  ('Ashley', 'Rodriguez', 1, NULL, 1),
-  ('Kevin', 'Tupik', 1, 1, 0),
-  ('Kunal', 'Singh', 2, NULL, 1),
-  ('Malia', 'Brown', 2, 3, 0),
-  ('Sarah', 'Lourd', 3, NULL, 1),
-  ('Tom', 'Allen', 3, 4, 0),
-  ('John', 'Doe', 4, NULL, 1),
-  ('Mike', 'Chan', 4, 4, 0);
+  ('Ashley',
+   'Rodriguez',
+    1,
+     NULL,
+      1),
+  ('Kevin', 'Tupik', 2, 1, 0),
+  ('Kunal', 'Singh', 3, NULL, 1),
+  ('Malia', 'Brown', 4, 3, 0),
+  ('Sarah', 'Lourd', 5, NULL, 1),
+  ('Tom', 'Allen', 6, 5, 0),
+  ('John', 'Doe', 7, NULL, 1),
+  ('Mike', 'Chan', 8, 7, 0);
